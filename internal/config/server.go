1d07eec1d238dbfe0aa31d9754eb54926bdfdd12
SingletonVD <singletonvd@mail.ru>
2026-08-12 11:44:37 +0300

    Read config from env

--- internal/config/server.go
+++ internal/config/server.go
@@ -1,6 +1,9 @@
 package config
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 type ServerConfig struct {
 	RunAddress      string
@@ -12,5 +15,14 @@ func InitServerConfig() *ServerConfig {
 	flag.StringVar(&serverConfig.RunAddress, "a", "localhost:8080", "address to bind server")
 	flag.StringVar(&serverConfig.BaseLinkAddress, "b", "http://localhost:8080", "base address for shortened link")
 	flag.Parse()
+
+	if envServerAddress := os.Getenv("SERVER_ADDRESS"); envServerAddress != "" {
+		serverConfig.RunAddress = envServerAddress
+	}
+
+	if envBaseUrl := os.Getenv("BASE_URL"); envBaseUrl != "" {
+		serverConfig.BaseLinkAddress = envBaseUrl
+	}
+
 	return serverConfig
 }
